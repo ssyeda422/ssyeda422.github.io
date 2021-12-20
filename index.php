8b14afb3a656e243ec27a274870ca56eae09334d
ssyeda422 <60491333+ssyeda422@users.noreply.github.com>
2021-12-20 01:26:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,1 @@
-<?php
-
-if($_POST["hidden"]) {
-    $recipient="saniyazsyeda@outlook.com";
-    $subject="Form to email message";
-    $sender=$_POST["name"];
-    $senderEmail=$_POST["_replyto"];
-    $message=$_POST["text"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    echo '<script>alert("Thank you! Your message has been sent.")</script>';
-}
-?>
-
 <?php include_once("index.html"); ?>
